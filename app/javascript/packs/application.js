f8bf76b65dd67225d4fa2c0c98a45b7f50f39581
Elias Glyptis <eliasglyptis@gmail.com>
2022-09-16 18:19:30 +0300

    Added Noty to imporve Devise error messages and notification to appear nicely.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// Noty for notifications
+window.Noty = require("noty")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
