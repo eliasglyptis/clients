56d36ce4e0301f168989fcb58bd4d69e8fa877c2
Elias Glyptis <eliasglyptis@gmail.com>
2022-09-16 19:50:39 +0300

    Created Clients controller with actions new create edit update and destroy

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,45 @@
+class ClientsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_client, except: [:new, :create, :destroy]
+  before_action :is_authorised, only: [:edit, :update, :destroy]
+
+  def new
+    @client = current_user.clients.build
+  end
+
+  def create
+    @client = current_user.clients.build(client_params)
+    if @client.save
+      redirect_to edit_client_path(@client), notice: "#{@client.title} has been added successfully"
+    else
+      # for dev mode to show all errors.
+      redirect_to request.referrer, flash: { error: @client.errors.full_messages }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+    def set_client
+      @client = Client.find(params[:id])
+    end
+
+    def is_authorised
+      redirect_to root_path, alert: "Hmm..not authorized for this action" unless current_user.id == @client.user_id
+    end
+
+    def client_params
+      params.require(:client).permit(:first_name, :last_name, :email)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
+  resources :clients
+  
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'register', sign_out: 'logout', edit: 'profile' }
 end
